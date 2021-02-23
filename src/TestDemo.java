ff19921e45b457f648afdf3fdd3c5f8eb6b32cb0
cnp.az <akylbek.zhumabayev@processing.kz>
2021-02-23 18:18:20 +0600

    test commit 2

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -3,5 +3,8 @@ public class TestDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(2, 3));
     }
 }
